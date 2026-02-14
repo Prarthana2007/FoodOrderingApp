ef79d278c681506bff6f61771a2ece0cfe5eb132
Prarthana <prarthanas1907@gmail.com>
2026-02-14 15:30:46 +0530

    Added Student class

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student{
+  private int studentId;
+  private String studentName;
+  private String phoneNumber;
+
+public Student(int studentId, String studentName, String phoneNumber){
+  this.studentId=studentId;
+  this.studentName=studentName;
+  this.phoneNumber=phoneNumber;
+public void login(){
+}
+public void viewMenu(){
+}
+public void placeOrder(){
+}
+public void viewOrder(){
+}
+}
