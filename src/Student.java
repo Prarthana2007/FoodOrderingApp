d691d834bdc34cdc4cccaa776ec0c5eefd6c2257
Prarthana <prarthanas1907@gmail.com>
2026-02-15 12:15:15 +0530

    Student
    Refactored Student class constructor and added methods for menu and order management.

--- src/Student.java
+++ src/Student.java
@@ -3,16 +3,17 @@ public class Student{
   private String studentName;
   private String phoneNumber;
 
-public Student(int studentId, String studentName, String phoneNumber){
-  this.studentId=studentId;
-  this.studentName=studentName;
-  this.phoneNumber=phoneNumber;
-public void login(){
-}
-public void viewMenu(){
-}
-public void placeOrder(){
-}
-public void viewOrder(){
-}
+  public Student(int studentId, String studentName, String phoneNumber){
+   this.studentId=studentId;
+   this.studentName=studentName;
+   this.phoneNumber=phoneNumber;
+  }
+  public void login(){
+  }
+  public void viewMenu(){
+  }
+  public void placeOrder(){
+  }
+  public void viewOrder(){
+  }
 }
