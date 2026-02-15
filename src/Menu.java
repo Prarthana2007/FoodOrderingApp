991a3b83b0ab4347192eb16e0d3f642450488ec6
Prarthana <prarthanas1907@gmail.com>
2026-02-15 12:25:21 +0530

    Menu

---
+++ src/Menu.java
@@ -0,0 +1,15 @@
+public class Menu{
+  private int menuId;
+
+  public Menu(int menuId){
+    this.menuId=menuId;
+    
+    public void addItem(){
+    }
+    public void removeItem(){
+    }
+    public void updateItems(){
+    }
+    public void getItems(){
+    }
+    
