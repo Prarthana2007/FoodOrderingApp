01122d7f049808f0cead316a01a270ba8a6f23be
Prarthana <prarthanas1907@gmail.com>
2026-02-15 12:12:15 +0530

    Canteen

---
+++ src/Canteen.java
@@ -0,0 +1,17 @@
+public classs Canteen{
+   private int canteenId;
+   private String canteenName;
+   private String location;
+
+  public Canteen(int canteenId, String canteenName, String location){
+    this.canteenId=canteenId;
+    this.canteenName=canteenName;
+    this.location=location;
+  }
+  public void updateMenu(){
+    }
+public void viewOrders(){
+  }
+public void confirmOrder(){
+  }
+}
